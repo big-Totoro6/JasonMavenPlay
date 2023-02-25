a4aa2adf2bcab4758433d183b4fbf552920d05ee
zhd <1304684527@qq.com>
2023-02-25 22:06:59 +0800

    feat: 使用SaToken进行认证 鉴权

--- oauth2-auth/src/main/java/com/jason/cloud/controller/AuthController.java
+++ oauth2-auth/src/main/java/com/jason/cloud/controller/AuthController.java
@@ -1,50 +1,58 @@
 package com.jason.cloud.controller;
 
-import com.jason.cloud.api.CommonResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * 权限测试
+ */
 @RestController
+@RequestMapping("/auth/")
 public class AuthController {
-    @GetMapping("/auth")
-    public CommonResult<String> test(){
-        return CommonResult.success("试试");
+    // 查询 permission 信息  ---- http://localhost:9601/auth/permission
+    @RequestMapping("permission")
+    public SaResult permission() {
+        // 获取：当前账号所拥有的权限集合
+        List<String> permissionList = StpUtil.getPermissionList();
+
+        // 判断：当前账号是否含有指定权限, 返回 true 或 false
+        boolean b = StpUtil.hasPermission("user.add");
+
+        // 校验：当前账号是否含有指定权限, 如果验证未通过，则抛出异常: NotPermissionException
+        StpUtil.checkPermission("user.add");
+
+        // 校验：当前账号是否含有指定权限 [指定多个，必须全部验证通过]
+        StpUtil.checkPermissionAnd("user.add", "user.delete", "user.get");
+
+        // 校验：当前账号是否含有指定权限 [指定多个，只要其一验证通过即可]
+        StpUtil.checkPermissionOr("user.add", "user.delete", "user.get");
+
+        return SaResult.data(StpUtil.getTokenInfo());
     }
 
-    @GetMapping("/adminAuth")
-    public CommonResult<String> adminAuth(){
-        return CommonResult.success("adminAuth");
-    }
+    // 查询 permission 信息  ---- http://localhost:9601/auth/rule
+    @RequestMapping("rule")
+    public SaResult rule() {
+        // 获取：当前账号所拥有的角色集合
+        StpUtil.getRoleList();
 
-    @GetMapping("/testAuth")
-    public CommonResult<String> testAuth(){
-        return CommonResult.success("testAuth");
-    }
+        // 判断：当前账号是否拥有指定角色, 返回 true 或 false
+        StpUtil.hasRole("super-admin");
 
-    @GetMapping("/product/{id}")
-    public String getProduct(@PathVariable String id) {
-        //for debug
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return "product id : " + id;
-    }
+        // 校验：当前账号是否含有指定角色标识, 如果验证未通过，则抛出异常: NotRoleException
+        StpUtil.checkRole("super-admin");
 
-    @GetMapping("/order/{id}")
-    public String getOrder(@PathVariable String id) {
-        //for debug
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return "order id : " + id;
-    }
+        // 校验：当前账号是否含有指定角色标识 [指定多个，必须全部验证通过]
+        StpUtil.checkRoleAnd("super-admin", "shop-admin");
 
-    @GetMapping("/ignore/getIgnore")
-    public String getIgnore() {
-        //for debug
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return "ignore";
+        // 校验：当前账号是否含有指定角色标识 [指定多个，只要其一验证通过即可]
+        StpUtil.checkRoleOr("super-admin", "shop-admin");
+
+
+        return SaResult.data(StpUtil.getTokenInfo());
     }
 }
--- oauth2-auth/src/main/java/com/jason/cloud/controller/UserController.java
+++ oauth2-auth/src/main/java/com/jason/cloud/controller/UserController.java
@@ -4,25 +4,42 @@ import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/user/")
 public class UserController {
+    public Logger global = Logger.getGlobal();
 
-
-    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    // 测试登录，浏览器访问： http://localhost:9601/user/doLogin?username=zhang&password=123456
     @RequestMapping("doLogin")
     public String doLogin(String username, String password) {
         // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
-        if("zhang".equals(username) && "123456".equals(password)) {
+        if ("zhang".equals(username) && "123456".equals(password)) {
             StpUtil.login(10001);
             return "登录成功";
         }
         return "登录失败";
     }
 
-    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    // 查询登录状态，浏览器访问： http://localhost:9601/user/isLogin
     @RequestMapping("isLogin")
     public String isLogin() {
+
+        // 获取当前会话的token值
+        String tokenValue = StpUtil.getTokenValue();
+        global.info(tokenValue);
+// 获取当前`StpLogic`的token名称
+
+        global.info(StpUtil.getTokenName());
+// 获取指定token对应的账号id，如果未登录，则返回 null
+
+        global.info((String) StpUtil.getLoginIdByToken(tokenValue));
+// 获取当前会话剩余有效期（单位：s，返回-1代表永久有效）
+
+        global.info(String.valueOf(StpUtil.getTokenTimeout()));
+// 获取当前会话的token信息参数
+        global.info(StpUtil.getTokenInfo().toString());
         return "当前会话是否登录：" + StpUtil.isLogin();
     }
 
